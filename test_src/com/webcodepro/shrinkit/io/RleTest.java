006eadf4109e0c596e132115d37001fc8d70f9ce
Robert Greene <robgreene@users.sourceforge.net>
2008-06-30 03:53:01 +0000

    Added RleOutputStream for RLE compression.

--- test_src/com/webcodepro/shrinkit/io/RleTest.java
+++ test_src/com/webcodepro/shrinkit/io/RleTest.java
@@ -1,5 +1,6 @@
 package com.webcodepro.shrinkit.io;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,23 +12,59 @@ import java.io.OutputStream;
  * @author robgreene@users.sourceforge.net
  */
 public class RleTest extends TestCaseHelper {
+	/**
+	 * Test the RleInputStream to verify decoding.
+	 */
 	public void testInputStream() throws IOException {
-		InputStream is = new RleInputStream(new LittleEndianByteInputStream(getPatternFileRle()));
+		InputStream is = new RleInputStream(new ByteArrayInputStream(getPatternFileRle()));
 		ByteArrayOutputStream os = new ByteArrayOutputStream();
 		copy(is,os);
 		byte[] expected = getPatternFileUncompressed();
 		byte[] actual = os.toByteArray();
 		assertEquals(expected, actual);
 	}
+	/**
+	 * Test the RleOutputStream to verify compression.
+	 */
+	public void testOutputStream() throws IOException {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		RleOutputStream os = new RleOutputStream(baos);
+		ByteArrayInputStream is = new ByteArrayInputStream(getPatternFileUncompressed());
+		copy(is,os);
+		byte[] expected = getPatternFileRle();
+		byte[] actual = baos.toByteArray();
+		assertEquals(expected, actual);
+	}
+	/**
+	 * Test the RleOutputStream with the escape character to ensure it <em>always</em> is encoded.
+	 * Note that a file with lots of 0xdb codes will grow.
+	 */
+	public void testOutputStreamEscapeCharacter() throws IOException {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		RleOutputStream os = new RleOutputStream(baos);
+		ByteArrayInputStream is = new ByteArrayInputStream(new byte[] { (byte)0xdb, (byte)0xdb, 0x00, (byte)0xdb });
+		copy(is,os);
+		byte[] expected = new byte[] { (byte)0xdb, (byte)0xdb, 0x01, 0x00, (byte)0xdb, (byte)0xdb, 0x00 };
+		byte[] actual = baos.toByteArray();
+		assertEquals(expected, actual);
+	}
 	
+	/**
+	 * Copy the input stream to the output stream.
+	 */
 	private void copy(InputStream is, OutputStream os) throws IOException {
 		int b = is.read();
 		while (b != -1) {
 			os.write(b);
 			b = is.read();
 		}
+		is.close();
+		os.close();
 	}
-	
+
+	/**
+	 * This the RLE compressed pattern file that happens to not compress via LZW.
+	 */
 	private byte[] getPatternFileRle() {
 		return new byte[] {
 			(byte)0xdb, 0x01, (byte)0xfd, 
@@ -49,6 +86,9 @@ public class RleTest extends TestCaseHelper {
 			(byte)0xdb, 0x11, (byte)0x97
 		};
 	}
+	/**
+	 * This is the uncompressed pattern file that happens to not compress via LZW.
+	 */
 	private byte[] getPatternFileUncompressed() {
 		byte[] data = new byte[4096];
 		int value = 0x01;
