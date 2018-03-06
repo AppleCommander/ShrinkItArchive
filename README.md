1c54b5ac163333ada46dcac2fa78d64ce91a1be9
Rob Greene <robgreene@gmail.com>
2018-03-05 21:48:48 -0600

    Migrating to Gradle; leaving the one failing Unit test as-is for now. #1.

---
+++ README.md
@@ -0,0 +1,5 @@
+# ShrinkIt Archive Library for Java
+
+This project is a direct off-shoot of the AppleCommander project and implements a pure Java API to manipulate ShrinkIt (NuFX) 
+archives ([Apple File Type Note $E0/8002](http://www.apple2online.com/web_documents/ft_e0.8002_shrinkit.pdf)).
+
--- src/test/java/com/webcodepro/shrinkit/io/NufxLzwTest.java
+++ src/test/java/com/webcodepro/shrinkit/io/NufxLzwTest.java
@@ -17,30 +17,30 @@ import com.webcodepro.shrinkit.ThreadRecord;
  */
 public class NufxLzwTest extends TestCaseHelper {
 	public void testNufxLzw1() throws IOException {
-		check("APPLE.II-LZW1.SHK", "APPLE.II", "APPLE.II.txt");
+		check("/APPLE.II-LZW1.SHK", "APPLE.II", "/APPLE.II.txt");
 	}
 	public void testNufxLzw2() throws IOException {
-		check("APPLE.II-LZW2.SHK", "APPLE.II", "APPLE.II.txt");
+		check("/APPLE.II-LZW2.SHK", "APPLE.II", "/APPLE.II.txt");
 	}
 	
 	public void testLargerFilesNufxLzw1() throws IOException {
-		check("PRODOS.MSTR-LZW1.SHK", "SYSUTIL.SYSTEM", "SYSUTIL.SYSTEM.bin");
-		check("PRODOS.MSTR-LZW1.SHK", "UTIL.0", "UTIL.0.bin");
-		check("PRODOS.MSTR-LZW1.SHK", "UTIL.1", "UTIL.1.bin");
-		check("PRODOS.MSTR-LZW1.SHK", "UTIL.2", "UTIL.2.bin");
+		check("/PRODOS.MSTR-LZW1.SHK", "SYSUTIL.SYSTEM", "/SYSUTIL.SYSTEM.bin");
+		check("/PRODOS.MSTR-LZW1.SHK", "UTIL.0", "/UTIL.0.bin");
+		check("/PRODOS.MSTR-LZW1.SHK", "UTIL.1", "/UTIL.1.bin");
+		check("/PRODOS.MSTR-LZW1.SHK", "UTIL.2", "/UTIL.2.bin");
 	}
 	public void testLargerFilesNufxLzw2() throws IOException {
-		check("PRODOS.MSTR-LZW2.SHK", "SYSUTIL.SYSTEM", "SYSUTIL.SYSTEM.bin");
-		check("PRODOS.MSTR-LZW2.SHK", "UTIL.0", "UTIL.0.bin");
-		check("PRODOS.MSTR-LZW2.SHK", "UTIL.1", "UTIL.1.bin");
-		check("PRODOS.MSTR-LZW2.SHK", "UTIL.2", "UTIL.2.bin");
+		check("/PRODOS.MSTR-LZW2.SHK", "SYSUTIL.SYSTEM", "/SYSUTIL.SYSTEM.bin");
+		check("/PRODOS.MSTR-LZW2.SHK", "UTIL.0", "/UTIL.0.bin");
+		check("/PRODOS.MSTR-LZW2.SHK", "UTIL.1", "/UTIL.1.bin");
+		check("/PRODOS.MSTR-LZW2.SHK", "UTIL.2", "/UTIL.2.bin");
 	}
 	
 	public void testUncompressedFiles() throws IOException {
-		check("UNCOMPRESSED.SHK", "APPLE.II-LZW1.SHK", "APPLE.II-LZW1.SHK");
-		check("UNCOMPRESSED.SHK", "APPLE.II-LZW2.SHK", "APPLE.II-LZW2.SHK");
-		check("UNCOMPRESSED.SHK", "PRODOS.MSTR-LZW1.SHK", "PRODOS.MSTR-LZW1.SHK");
-		check("UNCOMPRESSED.SHK", "PRODOS.MSTR-LZW2.SHK", "PRODOS.MSTR-LZW2.SHK");
+		check("/UNCOMPRESSED.SHK", "APPLE.II-LZW1.SHK", "/APPLE.II-LZW1.SHK");
+		check("/UNCOMPRESSED.SHK", "APPLE.II-LZW2.SHK", "/APPLE.II-LZW2.SHK");
+		check("/UNCOMPRESSED.SHK", "PRODOS.MSTR-LZW1.SHK", "/PRODOS.MSTR-LZW1.SHK");
+		check("/UNCOMPRESSED.SHK", "PRODOS.MSTR-LZW2.SHK", "/PRODOS.MSTR-LZW2.SHK");
 	}
 	
 	/**
