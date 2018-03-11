216c73d32bbb6ff95de8c94fcda5159b0af61fb9
Rob Greene <robgreene@gmail.com>
2018-03-11 17:34:28 -0500

    Missed this in the AC2 merge.

---
+++ src/main/java/com/webcodepro/shrinkit/HeaderConstants.java
@@ -0,0 +1,14 @@
+package com.webcodepro.shrinkit;
+
+/**
+ * Handy reference for constant values based on header blocks.
+ * These are likely user interface oriented.
+ * 
+ * @author rob
+ */
+public interface HeaderConstants {
+	public static final String[] FILE_SYS = {
+		"Reserved", "ProDOS/SOS", "DOS 3.3", "DOS 3.2", "Apple II Pascal", "Macintosh HFS", "Macintosh MFS",
+		"Lisa File System", "Apple CP/M", "Reserved", "MS-DOS", "High Sierra", "ISO 9660", "AppleShare"
+		};
+}
