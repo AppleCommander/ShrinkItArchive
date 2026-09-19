536c2ce3b892ee653998b43c3311bc28b29e4c62
a2geek <robgreene@gmail.com>
2026-09-19 14:16:08 -0500

    Adding license headers.

--- src/main/java/org/applecommander/shrinkit/Utility.java
+++ src/main/java/org/applecommander/shrinkit/Utility.java
@@ -1,3 +1,21 @@
+/*
+ * ShrinkItArchive
+ * Copyright (C) 2026  Rob Greene
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see
+ * <https://www.gnu.org/licenses/>.
+ */
 package org.applecommander.shrinkit;
 
 /// Some utility type methods that don't appear to belong anywhere else.
